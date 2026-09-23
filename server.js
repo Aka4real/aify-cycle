365cba39fe83ff66a320bae063df1555c619497c
Aka4real <enwedogift@gmail.com>
2026-09-24 00:17:49 +0100

    feat(branding): add high-res app icon, favicon suite, PWA manifest, and Open Graph link preview card

--- server.js
+++ server.js
@@ -43,8 +43,10 @@ const MIME_TYPES = {
   '.json': 'application/json; charset=utf-8',
   '.png': 'image/png',
   '.jpg': 'image/jpeg',
+  '.jpeg': 'image/jpeg',
   '.svg': 'image/svg+xml',
-  '.ico': 'image/x-icon'
+  '.ico': 'image/x-icon',
+  '.webmanifest': 'application/manifest+json'
 };
 
 /**
